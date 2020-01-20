bab36660256b604993c76482ed4552b848e14401
謝長霖 <singularity@Aaron-MBP.local>
2020-01-21 00:06:07 +0800

    Hello

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors')
 
 const app = express(cors());
 const port = process.env.PORT || 3000;
-app.listen(3000, () => { 
-    console.log(`Starting sever at ${port}`) 
+app.listen(port, () => {
+    console.log(`Starting sever at ${port}`)
 });
 
 app.use(express.static('public')); //show public dir
@@ -22,52 +22,5 @@ app.post('/post', async (request, response) => {
     const fetch_response = await fetch(url);
     fetch_response.type = 'basic'
     const rr = await fetch_response.json();
-    // console.log(fetch_response.status);
-    // console.log(fetch_response.statusText);
-    // console.log(fetch_response.redirected);
-    // console.log(fetch_response.ok);
-    // console.log(fetch_response.url);
-    // console.log(fetch_response.type);
-    // console.log('hI',rr['List'][2]['teacher']);
     response.json(rr['List']);
-    // response.text(rr['List'][0]['teacher'])
-    // response.send('Success');
-    
 });
-// app.get('/get', async (request, response) => {
-//     // data = request.body;
-//     // console.log(request);
-//     const url = new URL('http://courseap.itc.ntnu.edu.tw/acadmOpenCourse/CofopdlCtrl');
-//     url.search = new URLSearchParams(data).toString();
-//     const fetch_response = await fetch(url);
-//     const rr = await fetch_response.json();
-//     console.log(rr['List'][2]['teacher']);
-//     response.json(rr['List'][2]);
-//     // response.text(rr['List'][0]['teacher'])
-//     // response.send('Success2');
-//     // response.json(data);
-// });
-// app.get('/get', (request, response) => {
-//     const url = new URL('http://courseap.itc.ntnu.edu.tw/acadmOpenCourse/CofopdlCtrl');
-//     url.search = new URLSearchParams(data).toString();
-//     catchUrl()
-//         .then(res => {
-//             // document.getElementById('poem').innerText = poem;
-//             console.log('yay');
-//             console.log(res['List'][2]['teacher']);
-//             const da = res['List'][2];
-//             response.json(da)
-//         })
-//         .catch(error => {
-//             console.log('error!');
-//             console.error(error);
-//         });
-//     async function catchUrl() {
-//         const response = await fetch(url);
-//         // document.getElementById('poem').innerText = await response.text();
-//         return await response.json();
-//     };
-//     // response.json(da);
-//     // console.log(da)
-//     // response.json(data);
-// });
